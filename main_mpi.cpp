7e5d5dbfe124580f6c8173483fcdbc9af3239da6
Yueqiu <ys3202>
2019-05-19 20:13:25 -0400

    update

--- main_mpi.cpp
+++ main_mpi.cpp
@@ -131,6 +131,7 @@ void printAllGrid(int *allgrid,int iteration) {
             cr = grid_val / (1000 * 1000);
             cg = (grid_val - cr * (1000 * 1000)) / 1000;
             cb = grid_val - cr * (1000 * 1000) - cg * 1000;
+            printf("%3d ");
         }
         printf("\n");
     }
